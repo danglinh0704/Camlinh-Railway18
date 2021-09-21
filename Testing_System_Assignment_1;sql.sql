e9a3d302e755a62fa7cacb50938d3702d5210d7e
danglinh0704 <camlinh.dang0704@gmail.com>
2021-09-22 00:40:38 +0900

    Moi tai file bt1

---
+++ Testing_System_Assignment_1;sql.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS Testing_System_Assignment_1;
+
+CREATE DATABASE Testing_System_Assignment_1;
+
+
+USE Testing_System_Assignment_1;
+
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY AUTO_INCREMENT,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Email Varchar(50)
+);
+
